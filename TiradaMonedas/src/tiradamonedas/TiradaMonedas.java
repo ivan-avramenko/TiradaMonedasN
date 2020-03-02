0e9d19563b7c115a020a265c719045b51853fa4f
Ivan Avramenko <33833331+ivan-avramenko@users.noreply.github.com>
2020-03-02 20:51:49 +0100

    5. cambiar cabecera del metodo

--- TiradaMonedas/src/tiradamonedas/TiradaMonedas.java
+++ TiradaMonedas/src/tiradamonedas/TiradaMonedas.java
@@ -85,10 +85,17 @@ public class TiradaMonedas {
             }
         }
 
-        Imprimir(contadorCaras, contadorCruces, maximoCarasDoblesConsecutivas, maximoCrucesDoblesConsecutivas);
+        Imprime(contadorCaras, contadorCruces, maximoCarasDoblesConsecutivas, maximoCrucesDoblesConsecutivas);
     }
 
-    public static void Imprimir(int contadorCaras, int contadorCruces, int maximoCarasDoblesConsecutivas, int maximoCrucesDoblesConsecutivas) {
+    /**
+     *
+     * @param contadorCaras the value of contadorCaras
+     * @param contadorCruces the value of contadorCruces
+     * @param maximoCarasDoblesConsecutivas the value of maximoCarasDoblesConsecutivas
+     * @param maximoCrucesDoblesConsecutivas the value of maximoCrucesDoblesConsecutivas
+     */
+    public static void Imprime(int contadorCaras, int contadorCruces, int maximoCarasDoblesConsecutivas, int maximoCrucesDoblesConsecutivas) {
         System.out.println("Caras: " + contadorCaras + ", Cruces: " + contadorCruces);
         System.out.println("Maximo de caras dobles consecutivas: " + maximoCarasDoblesConsecutivas);
         System.out.println("Maximo de cruces dobles consecutivas: " + maximoCrucesDoblesConsecutivas);
